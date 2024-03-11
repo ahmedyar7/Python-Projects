a4795d4418f4d3d752a3130fc922c991d0229994
ahmedyar7 <ahmedyar778@gmail.com>
2024-03-12 02:40:10 +0500

    50 states of usa

--- 50 States of USA/main.py
+++ 50 States of USA/main.py
@@ -1,35 +1,35 @@
-import turtle
+# TODO: Adding the score counter
+
+from turtle import Turtle, Screen
 import pandas
 
+turtle = Turtle()
+screen = Screen()
 
-screen = turtle.Screen()
-screen.title("U.S States GAME")
-image = "blank_states_img.gif"
-screen.addshape(image)
-turtle.shape(image)
+img = "blank_states_img.gif"
+
+screen.addshape(img)
+turtle.shape(img)
 
 
 data = pandas.read_csv("50_states.csv")
-all_states = data.state.to_list()
-guessed_states = []
+states_names = data.state.tolist()
 
-while len(guessed_states) < 50:
-    answer_states = screen.textinput(
-        title=f"{len(guessed_states)}/50 States are correct",
-        prompt="What is another states name",
+correct_names = []
+
+while len(correct_names) < 50:
+    guessed_names = screen.textinput(
+        title=f"50 States of USA {len(correct_names)}/50 ", prompt="Guess the next one?"
     ).title()
 
-    if answer_states == "Exit":
-        missed_states = [state for state in all_states if state not in guessed_states]
-        new_data = pandas.DataFrame(missed_states)
-        new_data.to_csv("States_to_learn.csv")
-        break
-
-    if answer_states in all_states:
-        guessed_states.append(answer_states)
-        t = turtle.Turtle()
+    if guessed_names in states_names:
+        t = Turtle()
         t.hideturtle()
         t.penup()
-        state_data = data[data.state == answer_states]
+        state_data = data[data.state == guessed_names]
         t.goto(int(state_data.x), int(state_data.y))
-        t.write(answer_states)
+        t.write(guessed_names)
+
+print(len(correct_names))
+
+screen.mainloop()
