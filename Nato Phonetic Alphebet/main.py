424d3228e18d83c72659af65a21b53a1e0274001
ahmedyar7 <ahmedyar778@gmail.com>
2024-04-08 22:48:57 +0500

    Handling the execption in the code

--- Nato Phonetic Alphebet/main.py
+++ Nato Phonetic Alphebet/main.py
@@ -1,6 +1,6 @@
 import pandas
 
-data = pandas.read_csv("nato_phonetic_alphabet.csv")
+data = pandas.read_csv("Nato Phonetic Alphebet/nato_phonetic_alphabet.csv")
 
 phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
 
@@ -10,11 +10,15 @@ while game_is_on:
 
     user_input = input("Enter the Word: ").upper()
 
-    final_list = [phonetic_dict[letters] for letters in user_input]
-    print(final_list)
+    try:
+        final_list = [phonetic_dict[letters] for letters in user_input]
+    except KeyError:
+        print("Please enter the words or letters")
+    else:
+        print(final_list)
 
-    player_input = input("Do you want to continue, Yes or No: ").upper()
+        player_input = input("Do you want to continue, Yes or No: ").upper()
 
-    if player_input == "NO":
-        game_is_on = False
-        print("Thank You ❤ ^ ❤")
+        if player_input == "NO":
+            game_is_on = False
+            print("Thank You ❤ ^ ❤")
