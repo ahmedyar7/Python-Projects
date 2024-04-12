fded35a72f2e0d8af408fc2ee83b10a19f3fae59
ahmedyar7 <ahmedyar778@gmail.com>
2024-04-12 10:26:04 +0500

    Finishing up the project

--- ISS Overhead/main.py
+++ ISS Overhead/main.py
@@ -1,14 +1,14 @@
 import requests
 import smtplib
 from datetime import datetime
+import time
 
-# TODO : Make a function that return boolean value called iss_overhead()
-# TODO: Then after getting hold of the lat and long of the ISS data compare it with the my own lat and long with margin of 5 degrees
-# TODO: After compairsinon then give or return true or false
+YOUR_EMAIL_SMTP_PROTOCOL = "smtp.gmail.com"
+YOUR_EMAIL = "youraddress@email.com"
+YOUR_PASSWORD = "your password "
 
-
-MY_LAT = 30.179840
-MY_LONG = 66.974976
+YOUR_LAT = 000000
+YOUR_LONG = 000000
 
 
 def iss_overhead():
@@ -20,32 +20,51 @@ def iss_overhead():
     iss_latitude = float(data["iss_position"]["latitude"])
     iss_longitude = float(data["iss_position"]["longitude"])
 
-    if (MY_LAT + 5 <= iss_latitude or MY_LAT - 5 <= iss_latitude) or (
-        MY_LONG + 5 <= iss_longitude or MY_LONG - 5 <= iss_longitude
+    # Condition to check wether ISS is over your head or not
+    if (YOUR_LAT + 5 <= iss_latitude or YOUR_LAT - 5 <= iss_latitude) or (
+        YOUR_LONG + 5 <= iss_longitude or YOUR_LONG - 5 <= iss_longitude
     ):
         return True
 
 
-# def is_night():
-#     """Checks the sun rise and set timing with the current time(hours) and return boolean value"""
+def is_night():
+    """Checks the sun rise and set timing with the current time(hours) and return boolean value"""
 
-#     api_parameters = {
-#         "lat": MY_LAT,
-#         "lng": MY_LONG,
-#         "formatted": 0,
-#     }
+    api_parameters = {
+        "lat": YOUR_LAT,
+        "lng": YOUR_LONG,
+        "formatted": 0,
+    }
 
-#     time_now = datetime.now().hour
+    time_now = datetime.now().hour
 
-#     response = requests.get(
-#         url="https://api.sunrise-sunset.org/json", params=api_parameters
-#     )
-#     response.raise_for_status()
-#     data = response.json()
+    response = requests.get(
+        url="https://api.sunrise-sunset.org/json", params=api_parameters
+    )
+    response.raise_for_status()
+    data = response.json()
 
-#     sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
-#     sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
+    sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
+    sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
 
-#     # Condition that check wether its night or not:
-#     if time_now >= sunset and time_now <= sunrise:
-#         return True
+    # Condition that check wether its night or not:
+    if time_now >= sunset and time_now <= sunrise:
+        return True
+
+
+SUBJECT = "ISS Is overhead Loop Up. "
+MESSAGE = ""
+MINS = 60  # in seconds
+
+while True:
+    time.sleep(MINS)
+    if iss_overhead() and is_night():
+        with smtplib.SMTP(YOUR_EMAIL_SMTP_PROTOCOL) as connection:
+            connection.starttls()
+            connection.login(YOUR_EMAIL, YOUR_PASSWORD)
+
+            connection.sendmail(
+                to_addrs=YOUR_EMAIL,
+                from_addr=YOUR_EMAIL,
+                msg=f"Subject:{SUBJECT}\n\n{MESSAGE}",
+            )
