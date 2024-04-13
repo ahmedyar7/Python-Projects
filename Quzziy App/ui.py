585539132c758cb74a12eca0f6fa26db3d138cbf
ahmedyar7 <ahmedyar778@gmail.com>
2024-04-13 11:44:38 +0500

    Populating the canvas with the question from the open trivia api

--- Quzziy App/ui.py
+++ Quzziy App/ui.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from quiz_brain import QuizBrain
 
 THEME_COLOR = "#375362"
 QNA_COORDINATES = (150, 125)
@@ -7,7 +8,9 @@ QNA_FONT = ("Ariel", 17, "italic")
 
 class QuizInterface:
 
-    def __init__(self) -> None:
+    def __init__(self, quiz_brain: QuizBrain) -> None:
+
+        self.quiz_brain = quiz_brain
 
         # * Window Setup:
         self.window = Tk()
@@ -21,7 +24,7 @@ class QuizInterface:
         # * Canvas setup:
         self.canvas = Canvas(width=300, height=250)
         self.question_text = self.canvas.create_text(
-            QNA_COORDINATES, text="Random", font=QNA_FONT
+            QNA_COORDINATES, text="Random", font=QNA_FONT, width=270
         )
         self.canvas.grid(row=1, column=0, columnspan=2, pady=20)
 
@@ -39,4 +42,13 @@ class QuizInterface:
         )
         self.false_button.grid(row=2, column=1)
 
+        # For getting the next Question
+        self.get_next_question()
+
         self.window.mainloop()
+
+    def get_next_question(self) -> str:
+        """Return the Question to canvas"""
+
+        self.q_text = self.quiz_brain.next_question()
+        self.canvas.itemconfig(self.question_text, text=self.q_text)
