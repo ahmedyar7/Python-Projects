16893d2eaccf4ee76ec874485691051eb80dc3fd
ahmedyar7 <ahmedyar778@gmail.com>
2024-04-14 12:28:50 +0500

    Getting hold of the relevent Weather data

--- Rain Alert/main.py
+++ Rain Alert/main.py
@@ -1,5 +1,6 @@
 import os
 from dotenv import load_dotenv, find_dotenv
+import requests
 
 dotenv_path = find_dotenv()
 load_dotenv(dotenv_path)
@@ -12,3 +13,19 @@ ACCOUNT_SID = os.getenv("ACCOUNT_SID")
 AUTH_TOKEN = os.getenv("AUTH_TOKEN")
 TWILIO_PHONENUMBER = os.getenv("TWILIO_PHONENUMBER")
 MY_PHONENUMBER = os.getenv("MY_PHONENUMBER")
+
+API_PARAMETERS = {
+    "lat": LATITUDE,
+    "lon": LONGITUDE,
+    "appid": API_KEY,
+    "cnt": 4,
+}
+
+response = requests.get(
+    url="https://api.openweathermap.org/data/2.5/forecast", params=API_PARAMETERS
+)
+data = response.json()
+
+
+for hour_data in data["list"]:
+    print(hour_data["weather"][0]["id"])
