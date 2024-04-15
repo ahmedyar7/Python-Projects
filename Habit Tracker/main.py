c824b0b013c2f43c7a5219cd1f0becacc4623287
ahmedyar7 <ahmedyar778@gmail.com>
2024-04-15 20:42:58 +0500

    Adding the user

--- Habit Tracker/main.py
+++ Habit Tracker/main.py
@@ -1,3 +1,24 @@
-from ui import UserInterface
+import requests
+from os import getenv
+from dotenv import find_dotenv, load_dotenv
 
-ui = UserInterface()
+dotenv_path = find_dotenv()
+load_dotenv(dotenv_path)
+
+TOKEN = getenv("TOKEN")
+USERNAME = getenv("USER_NAME")
+
+
+# TODO: Make the user endpoint and user parameter on the habit trackers
+api_endpoint = "https://pixe.la/v1/users"
+
+user_data = {
+    "token": TOKEN,
+    "username": USERNAME,
+    "agreeTermsOfService": "yes",
+    "notMinor": "yes",
+}
+
+
+response = requests.post(url=f"{api_endpoint}", json=user_data)
+print(response.raise_for_status)
--- Habit Tracker/ui.py
+++
@@ -1,9 +0,0 @@
-from tkinter import *
-
-
-class UserInterface:
-
-    def __init__(self) -> None:
-        self.window = Tk()
-
-        self.window.mainloop()
