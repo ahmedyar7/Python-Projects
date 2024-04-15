fb01990d819595b24730b9654a846a8c0791bbcd
ahmedyar7 <ahmedyar778@gmail.com>
2024-04-15 14:28:09 +0500

    Dropping the plan for the SMTP lib and going for the twilio instead

--- Tesla News Teller/main.py
+++ Tesla News Teller/main.py
@@ -1,6 +1,8 @@
 import os
 from dotenv import load_dotenv, find_dotenv
 from requests import get
+import smtplib
+from html import unescape
 
 
 dotenv_path = find_dotenv()
@@ -10,6 +12,7 @@ load_dotenv(dotenv_path)
 # Getting hold of Environment Variable:
 API_KEY = os.getenv("API_KEY")
 
+SMTP_LIVE_SERVER = os.getenv("SMTP_LIVE_SERVER")
 MY_EMAIL = os.getenv("MY_EMAIL")
 YOUR_EMAIL = os.getenv("YOUR_EMAIL")
 PASSOWRD = os.getenv("PASSOWRD")
@@ -26,15 +29,30 @@ article = get(url=API_ENDPOINT, params=parameters)
 article.raise_for_status
 
 article_data = article.json().get("articles")
-first_three_articles = article_data[:3]
 
 # TODO Import the smtplib and make smtp live server and send mail
 
+i = 0
+i += 1
 
-for i in range(3):
-    source_name = article_data[i].get("source").get("name")
-    article_author = article_data[i].get("author")
-    article_title = article_data[i].get("title")
-    article_description = article_data[i].get("description")
 
-    message = f"Source: {source_name} \n Author: {article_author} \n Title: {article_title} \n Description: {article_description}"
+source_name = article_data[i].get("source").get("name")
+article_author = article_data[i].get("author")
+article_title = article_data[i].get("title")
+article_description = article_data[i].get("description")
+
+MESSAGE = unescape(
+    f"Source:{source_name}\nAuthor:{article_author}\nTitle:{article_title}\nDescription:{article_description}"
+)
+
+with smtplib.SMTP(SMTP_LIVE_SERVER) as connection:
+    connection.starttls()
+    connection.login(MY_EMAIL, PASSOWRD)
+
+    connection.sendmail(
+        to_addrs=YOUR_EMAIL,
+        from_addr=MY_EMAIL,
+        msg=f"Subject:Tesla Alert\n\n{MESSAGE}",
+    )
+
+print("SUCCESS")
