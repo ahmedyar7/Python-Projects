32f243f30f5f08552df1f08f94311bae0595b982
ahmedyar7 <ahmedyar778@gmail.com>
2024-04-15 13:20:03 +0500

    Fetching the data from the API

---
+++ Tesla News Teller/main.py
@@ -0,0 +1,31 @@
+import os
+from dotenv import load_dotenv, find_dotenv
+from requests import get
+
+
+dotenv_path = find_dotenv()
+load_dotenv(dotenv_path)
+
+
+# Getting hold of Environment Variable:
+API_KEY = os.getenv("API_KEY")
+
+MY_EMAIL = os.getenv("MY_EMAIL")
+YOUR_EMAIL = os.getenv("YOUR_EMAIL")
+PASSOWRD = os.getenv("PASSOWRD")
+
+# TODO : Get hold of all articles of yesterday
+# TODO: Get hold of only 2 articles from yesterday
+
+API_ENDPOINT = "https://newsapi.org/v2/everything"
+COMPANY_NAME = "Tesla"
+
+parameters = {
+    "apiKey": API_KEY,
+    "q": COMPANY_NAME,
+}
+
+article = get(url=API_ENDPOINT, params=parameters)
+article.raise_for_status
+
+article_data = article.json()
