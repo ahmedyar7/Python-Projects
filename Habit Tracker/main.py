286868960398c8aed12ac3746b579c7e5f01b5de
ahmedyar7 <ahmedyar778@gmail.com>
2024-04-15 20:53:15 +0500

    posting request for creating the graphs

--- Habit Tracker/main.py
+++ Habit Tracker/main.py
@@ -7,9 +7,9 @@ load_dotenv(dotenv_path)
 
 TOKEN = getenv("TOKEN")
 USERNAME = getenv("USER_NAME")
+GRAPH_ID = getenv("GRAPH_ID")
 
 
-# TODO: Make the user endpoint and user parameter on the habit trackers
 api_endpoint = "https://pixe.la/v1/users"
 
 user_data = {
@@ -20,5 +20,25 @@ user_data = {
 }
 
 
-response = requests.post(url=f"{api_endpoint}", json=user_data)
-print(response.raise_for_status)
+# response = requests.post(url=f"{api_endpoint}", json=user_data)
+# print(response.raise_for_status)
+
+# TODO: Make a graph on the pixels
+HEADERS = {
+    "X-USER-TOKEN": TOKEN,
+}
+
+graph_endpoint_data = {
+    "X-USER-TOKEN": TOKEN,
+    "id": GRAPH_ID,
+    "name": "Reading",
+    "unit": "commit",
+    "type": "int",
+    "color": "ajisai",
+}
+
+graph_endpoint = f"{api_endpoint}/{USERNAME}/graphs"
+
+response = requests.post(url=graph_endpoint, json=graph_endpoint_data, headers=HEADERS)
+
+print(response.text)
