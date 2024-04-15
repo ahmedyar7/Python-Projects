e5ecef25a5510e59377eff37bb4eff4fcad33b67
ahmedyar7 <ahmedyar778@gmail.com>
2024-04-15 14:31:40 +0500

    Adding the Twilio credential

--- Tesla News Teller/main.py
+++ Tesla News Teller/main.py
@@ -12,10 +12,11 @@ load_dotenv(dotenv_path)
 # Getting hold of Environment Variable:
 API_KEY = os.getenv("API_KEY")
 
-SMTP_LIVE_SERVER = os.getenv("SMTP_LIVE_SERVER")
-MY_EMAIL = os.getenv("MY_EMAIL")
-YOUR_EMAIL = os.getenv("YOUR_EMAIL")
-PASSOWRD = os.getenv("PASSOWRD")
+ACCOUNT_SID = os.getenv("ACCOUNT_SID")
+AUTH_TOKEN = os.getenv("AUTH_TOKEN")
+
+TWILIO_PHONENUMBER = os.getenv("TWILIO_PHONENUMBER")
+MY_PHONENUMBER = os.getenv("MY_PHONENUMBER")
 
 API_ENDPOINT = "https://newsapi.org/v2/everything"
 COMPANY_NAME = "Tesla"
@@ -30,29 +31,12 @@ article.raise_for_status
 
 article_data = article.json().get("articles")
 
-# TODO Import the smtplib and make smtp live server and send mail
 
-i = 0
-i += 1
+daily = 0
+daily += 1
+source_name = article_data[daily].get("source").get("name")
+article_author = article_data[daily].get("author")
+article_title = article_data[daily].get("title")
+article_description = article_data[daily].get("description")
 
-
-source_name = article_data[i].get("source").get("name")
-article_author = article_data[i].get("author")
-article_title = article_data[i].get("title")
-article_description = article_data[i].get("description")
-
-MESSAGE = unescape(
-    f"Source:{source_name}\nAuthor:{article_author}\nTitle:{article_title}\nDescription:{article_description}"
-)
-
-with smtplib.SMTP(SMTP_LIVE_SERVER) as connection:
-    connection.starttls()
-    connection.login(MY_EMAIL, PASSOWRD)
-
-    connection.sendmail(
-        to_addrs=YOUR_EMAIL,
-        from_addr=MY_EMAIL,
-        msg=f"Subject:Tesla Alert\n\n{MESSAGE}",
-    )
-
-print("SUCCESS")
+MESSAGE = f"Source:{source_name}\nAuthor:{article_author}\nTitle:{article_title}\nDescription:{article_description}"
