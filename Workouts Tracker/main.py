1eb95ae4b7b6c3d9e90215e9b5d8fa081ca14ded
ahmedyar7 <ahmedyar778@gmail.com>
2024-04-16 19:09:55 +0500

    Adding the workout entry function

--- Workouts Tracker/main.py
+++ Workouts Tracker/main.py
@@ -1,6 +1,7 @@
 from dotenv import find_dotenv, load_dotenv
 from os import getenv
 import requests
+from datetime import datetime as dt
 
 dotenv_path = find_dotenv()
 load_dotenv(dotenv_path)
@@ -13,6 +14,9 @@ WORKOUTS_API_KEY = getenv("WORKOUTS_API_KEY")
 SHEETY_API_TOKEN = getenv("SHEETY_API_TOKEN")
 SHEETY_API_URL = getenv("SHEETY_API_URL")
 
+# print(SHEETY_API_URL)
+# print(SHEETY_API_TOKEN)
+
 # Environment Variable -> Personal
 WEIGHT_KG = getenv("WEIGHT_KG")
 HEIGHT_CM = getenv("HEIGHT_CM")
@@ -23,7 +27,7 @@ AGE = getenv("AGE")
 WORKOUT_URL = "https://trackapi.nutritionix.com/v2/natural/exercise"
 
 
-def add_workout():
+def add_workout(text):
     """This function will add the workouts"""
 
     headers = {
@@ -39,5 +43,49 @@ def add_workout():
     }
 
     response = requests.post(url=WORKOUT_URL, json=body, headers=headers)
-    workout = response.json()["exercises"]
-    return workout
+    return response.json()["exercises"]
+    # return workout
+
+
+print(add_workout(text="I ran 3 miles"))
+
+# data_dict = {
+#     "tag_id": 317,
+#     "user_input": "ran",
+#     "duration_min": 10.01,
+#     "met": 9.8,
+#     "nf_calories": 114.45,
+#     "photo": {
+#         "highres": "https://d2xdmhkmkbyw75.cloudfront.net/exercise/317_highres.jpg",
+#         "thumb": "https://d2xdmhkmkbyw75.cloudfront.net/exercise/317_thumb.jpg",
+#         "is_user_uploaded": False,
+#     },
+#     "compendium_code": 12050,
+#     "name": "running",
+#     "description": "None",
+#     "benefits": "None",
+# }
+
+# TODO: Add the worout in the google sheet by using the shetty:
+
+
+def add_entry(entry):
+
+    body = {
+        "workout": {
+            "date": dt.now().strftime("%Y-%m-%d"),
+            "time": dt.now().strftime("%I-%M-%p"),
+            "exercise": entry["user_input"].capitalize(),
+            "duration": f"{entry['duration_min']} min",
+            "calories": f"{entry['nf_calories']} kcal",
+        }
+    }
+    print(body)
+
+    headers = {"Authorization": f"Bearer {SHEETY_API_TOKEN}"}
+    response = requests.post(url=SHEETY_API_URL, json=body, headers=headers)
+    response.raise_for_status()
+    print(response.content)
+
+
+# add_entry(entry=data_dict)
