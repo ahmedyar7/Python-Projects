b1c5c58f321d2f6624431a164636c056a25735e5
ahmedyar7 <ahmedyar778@gmail.com>
2024-04-20 18:44:57 +0500

    Finishing up the project

--- Amazon Price Tracker/main.py
+++ Amazon Price Tracker/main.py
@@ -3,6 +3,7 @@ from dotenv import find_dotenv, load_dotenv
 from requests import get
 from bs4 import BeautifulSoup
 import smtplib
+from email.message import EmailMessage
 
 dotenv_path = find_dotenv()
 load_dotenv(dotenv_path)
@@ -31,4 +32,20 @@ soup = BeautifulSoup(markup=data, features="html.parser")
 price = soup.find(name="span", class_="a-size-base a-color-secondary").text
 formmated_price = price.lstrip().replace("$", "")
 
+
 book_price = float(formmated_price)
+desired_price = 10.00
+
+# Creating the Email Object:-
+message = EmailMessage()
+message["Subject"] = "Amazon Price Tracker 🔍🔎"
+message.set_content("The Price has Dropped 📉📉")
+message["From"] = FROM_EMAIL
+message["To"] = TO_EMAIL
+
+
+if book_price <= desired_price:
+    with smtplib.SMTP(host=SMTP_SEVER, port=587) as connection:
+        connection.starttls()
+        connection.login(TO_EMAIL, PASSWORD)
+        connection.send_message(message)
