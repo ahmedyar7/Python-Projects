4e99bda63bcc9e1f81aae762b0ac691c4252ee89
ahmedyar7 <ahmedyar778@gmail.com>
2024-04-20 18:23:42 +0500

    Getting hold of the price

--- Amazon Price Tracker/main.py
+++ Amazon Price Tracker/main.py
@@ -17,3 +17,18 @@ TO_EMAIL = getenv("TO_EMAIL")
 FROM_EMAIL = getenv("FROM_EMAIL")
 PASSWORD = getenv("PASSWORD")
 SMTP_SEVER = getenv("SMTP_SEVER")
+
+HEADER = {
+    "User-Agent": USER_AGENT,
+    "Accept_Language": ACCEPT_LANGUAGE,
+}
+
+response = get(url=URL, headers=HEADER)
+data = response.text
+
+soup = BeautifulSoup(markup=data, features="html.parser")
+
+price = soup.find(name="span", class_="a-size-base a-color-secondary").text
+formmated_price = price.lstrip().replace("$", "")
+
+book_price = float(formmated_price)
