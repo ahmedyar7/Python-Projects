62e511a3394f9811772c5d1be497ddbd8f2fe69a
ahmedyar7 <ahmedyar778@gmail.com>
2024-04-20 09:35:19 +0500

    Finishing up the project

--- 100_Movies_to_watch/main.py
+++ 100_Movies_to_watch/main.py
@@ -1,12 +1,30 @@
 from bs4 import BeautifulSoup
 import requests
 
+URL = "https://www.empireonline.com/movies/features/best-movies-2/"
+MARKUP_PARSER = "html.parser"
+
 
 response = requests.get(
-    url="https://www.empireonline.com/movies/features/best-movies-2/"
+    url="",
 )
 data = response.text
 
-soup = BeautifulSoup(markup=data, features="html.parser")
-# <h3 class="listicleItem_listicle-item__title__BfenH">99) Groundhog Day</h3>
-first_movie = soup.find(name="h3", class_="listicleItem_listicle-item__title__BfenH")
+soup = BeautifulSoup(
+    markup=data,
+    features=MARKUP_PARSER,
+)
+movies = soup.find_all(
+    name="h3",
+    class_="listicleItem_listicle-item__title__BfenH",
+)
+
+all_movies = []
+for movie in movies:
+    all_movies.append(movie.get_text())
+
+all_films = all_movies[::-1]  # Reversing the list of movies to start from 1
+
+with open(file="100_movies_to_watch.txt", mode="a") as file:
+    for movie in all_films:
+        file.write(f"{movie}\n")
