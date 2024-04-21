f0067b2564467d1cd11ece1da0a16afa03ae00ce
ahmedyar7 <ahmedyar778@gmail.com>
2024-04-21 09:07:08 +0500

    Getting hold of the packages and the module

---
+++ Cookie_Clicker/main.py
@@ -0,0 +1,11 @@
+# TODO: Get hold of all the required packeages and the modules and get a window to show up
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_experimental_option("detach", True)
+
+driver = webdriver.Chrome(options=chrome_options)
+
+driver.get("https://orteil.dashnet.org/experiments/cookie/")
