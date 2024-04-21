28504230089acd8f9198ac83b52c3c4dc9bfed62
ahmedyar7 <ahmedyar778@gmail.com>
2024-04-21 09:20:07 +0500

    Getting hold of the upgarde items prices

--- Cookie_Clicker/main.py
+++ Cookie_Clicker/main.py
@@ -1,5 +1,3 @@
-# TODO: Get hold of all the required packeages and the modules and get a window to show up
-
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
@@ -9,3 +7,21 @@ chrome_options.add_experimental_option("detach", True)
 driver = webdriver.Chrome(options=chrome_options)
 
 driver.get("https://orteil.dashnet.org/experiments/cookie/")
+
+cookie = driver.find_element(by=By.CSS_SELECTOR, value="#cookie")
+
+# TODO: Get hold of the store items in a list
+store_items = []
+items = driver.find_elements(by=By.CSS_SELECTOR, value="#store div b")
+
+for item in items:
+    element_text = item.text
+    if element_text != "":
+        cost = element_text.split("-")[1].strip().replace(",", "")
+        store_items.append(cost)
+
+print(store_items)
+
+
+# while True:
+#     cookie.click()
