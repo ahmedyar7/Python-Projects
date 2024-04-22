8dd8fbfba3bd51016d68d5e3175ce20aff4dd6e1
ahmedyar7 <ahmedyar778@gmail.com>
2024-04-22 23:32:42 +0500

    Getting hold of the upload and the download speed

--- Twitter_Bot/main.py
+++ Twitter_Bot/main.py
@@ -11,3 +11,48 @@ load_dotenv(dotenv_path=find_dotenv())
 TWITTER_EMAIL = getenv("TWITTER_EMAIL")
 TWITTER_USERNAME = getenv("TWITTER_USERNAME")
 TWITTER_PASSWORD = getenv("TWITTER_PASSWORD")
+
+# Setting up Selenium:
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_experimental_option("detach", True)
+
+driver = webdriver.Chrome(options=chrome_options)
+
+upload_speed = 0
+download_speed = 0
+
+
+class TwitterNetBot:
+
+    def get_internet_speed():
+        """This will get the internet speed"""
+
+        driver.get(url="https://www.speedtest.net/")
+        time.sleep(60)
+
+        go_button = driver.find_element(
+            by=By.XPATH,
+            value="//*[@id='container']/div/div[3]/div/div/div/div[2]/div[3]/div[1]/a/span[4]",
+        )
+        go_button.click()
+        print("GOT CLICKED")
+
+        time.sleep(60)
+
+        global upload_speed, download_speed
+
+        upload_speed = driver.find_element(
+            by=By.XPATH,
+            value="//*[@id='container']/div/div[3]/div/div/div/div[2]/div[3]/div[3]/div/div[3]/div/div/div[2]/div[1]/div[2]/div/div[2]/span",
+        ).text
+        download_speed = driver.find_element(
+            by=By.XPATH,
+            value="//*[@id='container']/div/div[3]/div/div/div/div[2]/div[3]/div[3]/div/div[3]/div/div/div[2]/div[1]/div[1]/div/div[2]/span",
+        ).text
+
+        print(download_speed)
+        print(upload_speed)
+
+
+bot = TwitterNetBot
+bot.get_internet_speed()
