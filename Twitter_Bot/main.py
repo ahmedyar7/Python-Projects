dc9a973e03973014739a039fc5a102e0209a2e2d
ahmedyar7 <ahmedyar778@gmail.com>
2024-04-23 00:24:02 +0500

    Twitter Login successfull

--- Twitter_Bot/main.py
+++ Twitter_Bot/main.py
@@ -2,6 +2,7 @@ from os import getenv
 from dotenv import find_dotenv, load_dotenv
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 import time
 
 load_dotenv(dotenv_path=find_dotenv())
@@ -53,6 +54,39 @@ class TwitterNetBot:
         print(download_speed)
         print(upload_speed)
 
+    def twitter_login():
+        """This would login to one's twitter account"""
+
+        driver.get(url="https://twitter.com/i/flow/login")
+
+        time.sleep(20)
+
+        email_login = driver.find_element(
+            by=By.XPATH,
+            value="//*[@id='layers']/div/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div/div/div/div[5]/label/div/div[2]/div/input",
+        )
+        email_login.send_keys(TWITTER_EMAIL)
+        email_login.send_keys(Keys.ENTER)
+
+        time.sleep(10)
+
+        username_login = driver.find_element(
+            by=By.XPATH,
+            value="//*[@id='layers']/div/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[1]/div/div[2]/label/div/div[2]/div/input",
+        )
+        username_login.send_keys(TWITTER_USERNAME)
+        username_login.send_keys(Keys.ENTER)
+
+        time.sleep(10)
+
+        password_login = driver.find_element(
+            by=By.XPATH,
+            value="//*[@id='layers']/div/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[1]/div/div/div[3]/div/label/div/div[2]/div[1]/input",
+        )
+        password_login.send_keys(TWITTER_PASSWORD)
+        password_login.send_keys(Keys.ENTER)
+
 
 bot = TwitterNetBot
-bot.get_internet_speed()
+# bot.get_internet_speed()
+bot.twitter_login()
