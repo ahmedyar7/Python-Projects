13aca7856faa7bd758e703174455852d35c22d81
ahmedyar7 <ahmedyar778@gmail.com>
2024-04-23 21:41:26 +0500

    Finishing up the project

--- Instagram_Follower_Bot/main.py
+++ Instagram_Follower_Bot/main.py
@@ -11,6 +11,8 @@ load_dotenv(dotenv_path=find_dotenv())
 EMAIL = getenv("EMAIL")
 PASSWORD = getenv("PASSWORD")
 
+ACCOUNT_USERNAME = "ahmedyar.7"
+
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option("detach", True)
 
@@ -20,6 +22,7 @@ class InstagramFollowerBot:
     def __init__(self) -> None:
         self.driver = webdriver.Chrome(options=chrome_options)
         self.instagram_login()
+        # self.get_follow()
 
     def instagram_login(self) -> None:
         """This will login to the instgram page"""
@@ -39,6 +42,27 @@ class InstagramFollowerBot:
         password_input.send_keys(Keys.ENTER)
 
         print("login success full")
+
+        sleep(3)
+
+        not_now_button = self.driver.find_element(
+            by=By.CLASS_NAME,
+            value="_a9--._ap36._a9_0",
+        )
+        not_now_button.click()
+
+    def get_follow(self):
+        """This will follow the instagram account"""
+
+        sleep(3)
+        self.driver.get(url=f"https://www.instagram.com/{ACCOUNT_USERNAME}")
+        sleep(3)
+
+        account_follow = self.driver.find_element(
+            by=By.XPATH,
+            value="/html/body/div[2]/div/div/div[2]/div/div/div[1]/div[1]/div[2]/div[2]/section/main/div/header/section/div[1]/div[2]/div/div/button",
+        )
+        account_follow.click()
 
 
 bot = InstagramFollowerBot()
