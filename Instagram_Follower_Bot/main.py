cc328b038e002b37974be23e6d2411542982741d
ahmedyar7 <ahmedyar778@gmail.com>
2024-05-02 15:16:10 +0500

    Working with selenium implinct wait rather then explict wait

--- Instagram_Follower_Bot/main.py
+++ Instagram_Follower_Bot/main.py
@@ -3,7 +3,6 @@ from dotenv import find_dotenv, load_dotenv
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from time import sleep
 
 load_dotenv(dotenv_path=find_dotenv())
 
@@ -11,7 +10,7 @@ load_dotenv(dotenv_path=find_dotenv())
 EMAIL = getenv("EMAIL")
 PASSWORD = getenv("PASSWORD")
 
-ACCOUNT_USERNAME = "ahmedyar.7"
+ACCOUNT_USERNAME = "zohakh67"
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option("detach", True)
@@ -21,14 +20,15 @@ class InstagramFollowerBot:
 
     def __init__(self) -> None:
         self.driver = webdriver.Chrome(options=chrome_options)
+        self.wait = self.driver.implicitly_wait(30)
         self.instagram_login()
-        # self.get_follow()
+        self.get_follow()
 
     def instagram_login(self) -> None:
         """This will login to the instgram page"""
 
         self.driver.get(url="https://www.instagram.com/")
-        sleep(7)
+        self.wait
 
         email_input = self.driver.find_element(
             by=By.XPATH, value="//*[@id='loginForm']/div/div[1]/div/label/input"
@@ -43,24 +43,24 @@ class InstagramFollowerBot:
 
         print("login success full")
 
-        sleep(3)
+        self.wait
 
-        not_now_button = self.driver.find_element(
-            by=By.CLASS_NAME,
-            value="_a9--._ap36._a9_0",
-        )
-        not_now_button.click()
+        # not_now_button = self.driver.find_element(
+        #     by=By.CLASS_NAME,
+        #     value="_a9--._ap36._a9_0",
+        # )
+        # not_now_button.click()
 
     def get_follow(self):
         """This will follow the instagram account"""
 
-        sleep(3)
+        self.wait
         self.driver.get(url=f"https://www.instagram.com/{ACCOUNT_USERNAME}")
-        sleep(3)
+        self.wait
 
         account_follow = self.driver.find_element(
             by=By.XPATH,
-            value="/html/body/div[2]/div/div/div[2]/div/div/div[1]/div[1]/div[2]/div[2]/section/main/div/header/section/div[1]/div[2]/div/div/button",
+            value="/html/body/div[2]/div/div/div[2]/div/div/div[1]/div[2]/section/main/div/header/section/div[3]/div/div/button/div",
         )
         account_follow.click()
 
